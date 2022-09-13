05c1a9750ca8cada01a3a7dd7e91eaa8495227e9
Wouter Steggerda <woutersteggerda@gmail.com>
2022-09-13 10:17:55 +0200

    add the no-interaction option

--- src/Commands/Professionals/CognitoSetup.php
+++ src/Commands/Professionals/CognitoSetup.php
@@ -8,14 +8,16 @@ use Vng\EvaCore\Services\Cognito\UserPoolService;
 
 class CognitoSetup extends Command
 {
-    protected $signature = 'professionals:setup {--s|silent}';
+    protected $signature = 'professionals:setup {--n|no-interaction}';
     protected $description = 'Make sure the userpool and it\'s client are setup';
 
     public function handle(): int
     {
         $this->output->writeln('Userpool name: ' . UserPoolService::getUserPoolName());
         CognitoService::ensureSetup();
-        $this->call('professionals:get-config', ['--silent' => $this->option('silent')]);
+        $this->call('professionals:get-config', [
+            '--no-interaction' => $this->option('no-interaction')
+        ]);
         return 0;
     }
 }
