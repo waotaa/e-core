ca3fff01a2c9a278666c13c362b05d40e1cb017e
Wouter Steggerda <woutersteggerda@gmail.com>
2022-10-18 16:03:19 +0200

    1142 - modifies periodic instrument notification message

--- src/Notifications/PeriodicInstrumentUpdate.php
+++ src/Notifications/PeriodicInstrumentUpdate.php
@@ -45,9 +45,18 @@ class PeriodicInstrumentUpdate extends Notification
     {
         $mailMessage = (new MailMessage)
             ->subject(__('Periodic instrument update'))
-            ->greeting(__('Hello') . ' ' . $notifiable->name);
+            ->greeting(__('Dear :manager', ['manager' => $notifiable->name]). ',');
 
         $mailMessage = $this->addTrackerInfo($mailMessage);
+
+        $mailMessage->line(__('This e-mail is a reminder to view the instruments and update them where necessary. In this way Eva remains current.'));
+        $mailMessage->line(__('What do you have to do'). ':');
+        $mailMessage->line('1.' . __('View the instrument'));
+        $mailMessage->line('2.' . __('Check whether the texts and links are still up to date and make any changes'));
+        $mailMessage->line('3.' . __('Save the instrument again'));
+        $mailMessage->line(__('Thanks for your cooperation') . '!');
+        $mailMessage->line(__('Sincerely') . ',');
+        $mailMessage->line('Instrumentengids Eva');
         return $mailMessage;
     }
 
@@ -74,18 +83,26 @@ class PeriodicInstrumentUpdate extends Notification
 
     public function addSignalForCurrentPeriod(MailMessage $message)
     {
-        $message->line(__('Signals that require direct attention'));
+        $message = $message->line(__('The following instruments have been added to the Eva instrument guide for quite some time.'));
 
         $this->trackers->each(function (InstrumentTracker $tracker) use (&$message) {
             if($tracker->instrumentHasExpired()) {
-                $message = $message->line(__("The instrument - :instrument - has expired", [
-                    'instrument' => $tracker->instrument->name
+                $message = $message->line(__("Instrument :instrument of :provider", [
+                    'instrument' => $tracker->instrument->name,
+                    'provider' => $tracker->instrument->provider?->name
                 ]));
+//                $message = $message->line(__("The instrument - :instrument - has expired", [
+//                    'instrument' => $tracker->instrument->name
+//                ]));
             }
             if($tracker->instrumentNeedsUpdate()) {
-                $message = $message->line(__('The instrument - :instrument - needs a revision', [
-                    'instrument' => $tracker->instrument->name
+                $message = $message->line(__("Instrument :instrument of :provider", [
+                    'instrument' => $tracker->instrument->name,
+                    'provider' => $tracker->instrument->provider?->name
                 ]));
+//                $message = $message->line(__('The instrument - :instrument - needs a revision', [
+//                    'instrument' => $tracker->instrument->name
+//                ]));
             }
         });
         return $message;
@@ -93,17 +110,27 @@ class PeriodicInstrumentUpdate extends Notification
 
     public function addSignalsForNextPeriod(MailMessage $message)
     {
-        $message->line(__('Expected signals for next period'));
+        $message = $message->line(__('The following instruments will be due for a revision in the coming period.'));
+
         $this->trackers->each(function (InstrumentTracker $tracker) use (&$message) {
             if ($tracker->instrumentWillExpireNextPeriod()) {
-                $message = $message->line(__('The instrument - :instrument - will expire next period', [
-                    'instrument' => $tracker->instrument->name
+                $message = $message->line(__("Instrument :instrument of :provider", [
+                    'instrument' => $tracker->instrument->name,
+                    'provider' => $tracker->instrument->provider?->name
                 ]));
+
+//                $message = $message->line(__('The instrument - :instrument - will expire next period', [
+//                    'instrument' => $tracker->instrument->name
+//                ]));
             }
             if ($tracker->instrumentNeedsUpdateNextPeriod()) {
-                $message = $message->line(__('The instrument - :instrument - will need a revision next period', [
-                    'instrument' => $tracker->instrument->name
+                $message = $message->line(__("Instrument :instrument of :provider", [
+                    'instrument' => $tracker->instrument->name,
+                    'provider' => $tracker->instrument->provider?->name
                 ]));
+//                $message = $message->line(__('The instrument - :instrument - will need a revision next period', [
+//                    'instrument' => $tracker->instrument->name
+//                ]));
             }
         });
         return $message;
