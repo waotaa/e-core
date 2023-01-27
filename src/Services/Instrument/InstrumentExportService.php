f01c9a88ff651d06c27a8bdf60c3ff8cafdec56b
Wouter Steggerda <woutersteggerda@gmail.com>
2023-01-27 10:44:12 +0100

    instrument export allows for custom items

--- src/Services/Instrument/InstrumentExportService.php
+++ src/Services/Instrument/InstrumentExportService.php
@@ -2,6 +2,7 @@
 
 namespace Vng\EvaCore\Services\Instrument;
 
+use Illuminate\Support\Collection;
 use Vng\EvaCore\ElasticResources\InstrumentResource;
 use Vng\EvaCore\Models\Instrument;
 use Vng\EvaCore\Services\ImExport\AbstractEntityExportService;
@@ -10,13 +11,28 @@ class InstrumentExportService extends AbstractEntityExportService
 {
     protected string $entity = 'instrument';
 
+    protected ?Collection $items = null;
+
     public function handle(): string
     {
-        $instruments = Instrument::all()
-            ->map(function(Instrument $instrument) {
-                $instrument->import_mark = $this->importMark;
-                return InstrumentResource::make($instrument)->toArray();
-            });
+        if (is_null($this->items)) {
+            $this->setItems(Instrument::all());
+        }
+
+        $instruments = $this->items->map(function(Instrument $instrument) {
+            $instrument->import_mark = $this->importMark;
+            return InstrumentResource::make($instrument)->toArray();
+        });
         return $this->createExportJson($instruments);
+    }
+
+    /**
+     * @param Collection|null $items
+     * @return InstrumentExportService
+     */
+    public function setItems($items)
+    {
+        $this->items = $items;
+        return $this;
     }
 }
