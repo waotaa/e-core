b2e1642e71a1a43c4e10ae64438e2940f819ce8f
Wouter Steggerda <woutersteggerda@gmail.com>
2023-10-20 10:34:23 +0200

    alternative attempt

--- src/Services/ElasticSearch/KibanaService.php
+++ src/Services/ElasticSearch/KibanaService.php
@@ -80,18 +80,18 @@ class KibanaService
 //                    alternative
                     'application' => 'kibana-.kibana',
                     'privileges' => [
-                        'dashboard.minimal_read',
-                        'dashboard.store_search_session',
-                        'dashboard.generate_report',
-                        'visualize.minimal_read',
-                        'visualize.generate_report',
+//                        'dashboard.minimal_read',
+//                        'dashboard.store_search_session',
+//                        'dashboard.generate_report',
+//                        'visualize.minimal_read',
+//                        'visualize.generate_report',
 
 //                        alternative
-//                        "feature_dashboard.minimal_read",
-//                        "feature_dashboard.store_search_session",
-//                        "feature_dashboard.generate_report",
-//                        "feature_visualize.minimal_read",
-//                        "feature_visualize.generate_report"
+                        "feature_dashboard.minimal_read",
+                        "feature_dashboard.store_search_session",
+                        "feature_dashboard.generate_report",
+                        "feature_visualize.minimal_read",
+                        "feature_visualize.generate_report"
                     ],
                     'resources' => [
                         'space:instrumenten',
