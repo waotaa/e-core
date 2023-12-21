4ba14e955821ed89fc878e2e290c4371d17f414d
Wouter Steggerda <woutersteggerda@gmail.com>
2023-12-21 11:06:55 +0100

    1421 - applies unique validation on organisation levell

--- src/Http/Validation/AddressValidation.php
+++ src/Http/Validation/AddressValidation.php
@@ -29,12 +29,14 @@ class AddressValidation extends ModelValidation
             'huisnummer' => [
                 Rule::unique('addresses', 'huisnummer')
                     ->where('postcode', $this->request->input('postcode'))
+                    ->where('organisation_id', $this->request->input('organisation_id'))
             ],
             'postcode' => [
                 'required',
                 'regex:/^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-z]{2}$/i',
                 Rule::unique('addresses', 'postcode')
                     ->where('huisnummer', $this->request->input('huisnummer'))
+                    ->where('organisation_id', $this->request->input('organisation_id'))
             ],
         ];
     }
@@ -45,6 +47,7 @@ class AddressValidation extends ModelValidation
             'huisnummer' => [
                 Rule::unique('addresses', 'huisnummer')
                     ->where('postcode', $this->request->input('postcode'))
+                    ->where('organisation_id', $this->request->input('organisation_id'))
                     ->ignore($model->id)
             ],
             'postcode' => [
@@ -52,6 +55,7 @@ class AddressValidation extends ModelValidation
                 'regex:/^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-z]{2}$/i',
                 Rule::unique('addresses', 'postcode')
                     ->where('huisnummer', $this->request->input('huisnummer'))
+                    ->where('organisation_id', $this->request->input('organisation_id'))
                     ->ignore($model->id)
             ],
         ];
