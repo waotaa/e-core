b87ef847b0d4dc0cc4561713e54d004fe4f16315
Wouter Steggerda <woutersteggerda@gmail.com>
2024-06-26 14:28:33 +0200

    adds log for syncing

--- src/Jobs/SyncResourceToElasticJob.php
+++ src/Jobs/SyncResourceToElasticJob.php
@@ -41,6 +41,7 @@ class SyncResourceToElasticJob extends ElasticJob
             ]);
             $this->updateAttemptStatusWithResult($result);
         } catch (NoNodesAvailableException $noNodesAvailableException) {
+            Log::warning('No nodes available exception');
             $this->release(20);
         } catch (\Exception $exception) {
             Log::debug('Sync failed', [
