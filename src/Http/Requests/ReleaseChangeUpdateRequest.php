a1794b1e6831969feff6e5f7483d97dcab01b8a7
Wouter Steggerda <woutersteggerda@gmail.com>
2024-07-03 11:52:04 +0200

    fixes release change update request getModel method

--- src/Http/Requests/ReleaseChangeUpdateRequest.php
+++ src/Http/Requests/ReleaseChangeUpdateRequest.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
 use Vng\EvaCore\Http\Validation\ReleaseChangeValidation;
-use Vng\EvaCore\Models\Release;
+use Vng\EvaCore\Models\ReleaseChange;
 use Vng\EvaCore\Repositories\ReleaseChangeRepositoryInterface;
 
 class ReleaseChangeUpdateRequest extends BaseFormRequest implements FormRequestInterface
@@ -20,11 +20,11 @@ class ReleaseChangeUpdateRequest extends BaseFormRequest implements FormRequestI
 
     public function rules(): array
     {
-        $release = $this->getReleaseChange();
-        if (!$release instanceof Release) {
-            throw new \Exception('Cannot derive release from route');
+        $releaseChange = $this->getReleaseChange();
+        if (!$releaseChange instanceof ReleaseChange) {
+            throw new \Exception('Cannot derive releaseChange from route');
         }
-        return ReleaseChangeValidation::make($this)->getUpdateRules($release);
+        return ReleaseChangeValidation::make($this)->getUpdateRules($releaseChange);
     }
 
     protected function getReleaseChange(): ?Model
