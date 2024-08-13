755b5aeb4a28f054a08153ce9ecf28744089daf2
Wouter Steggerda <woutersteggerda@gmail.com>
2024-08-13 13:49:46 +0200

    updates resource

--- src/ElasticResources/DownloadResource.php
+++ src/ElasticResources/DownloadResource.php
@@ -12,16 +12,13 @@ class DownloadResource extends ElasticResource
 
     public function toArray()
     {
-        $cdn = config('filesystems.cdn');
-        $url = $cdn ? Str::finish($cdn, '/') . $this->url : $this->url;
-
         return [
             'id' => $this->id,
             'created_at' => $this->formatDate($this->created_at),
             'updated_at' => $this->formatDate($this->updated_at),
 
             'label' => $this->label,
-            'url' => $url,
+            'url' => $this->cdnUrl,
             'filename' => $this->filename,
 
             'instrument' => InstrumentResource::one($this->whenLoaded('instrument')),
