5b7ee0dc76eb88fffca750601d850339525ce498
Wouter Steggerda <woutersteggerda@gmail.com>
2024-11-26 17:35:21 +0100

    public instrument syncing in batches

--- src/Commands/Elastic/SyncPublicInstruments.php
+++ src/Commands/Elastic/SyncPublicInstruments.php
@@ -5,10 +5,13 @@ namespace Vng\EvaCore\Commands\Elastic;
 use Illuminate\Console\Command;
 use Vng\EvaCore\ElasticResources\Original\Shared\InstrumentResource;
 use Vng\EvaCore\Jobs\ElasticPublic\RemoveResourceFromPublicElasticJob;
+use Vng\EvaCore\Jobs\ElasticPublic\SyncBulkResourcesToPublicElasticJob;
 use Vng\EvaCore\Jobs\ElasticPublic\SyncResourceToPublicElasticJob;
 use Vng\EvaCore\Models\Instrument;
+use Vng\EvaCore\Models\SyncAttempt;
 use Vng\EvaCore\Repositories\InstrumentRepositoryInterface;
 use Vng\EvaCore\Services\ElasticSearch\ElasticPublicClientBuilder;
+use Vng\EvaCore\Services\ElasticSearch\SyncAttemptFactory;
 
 class SyncPublicInstruments extends Command
 {
@@ -33,28 +36,26 @@ class SyncPublicInstruments extends Command
         /** @var InstrumentRepositoryInterface $instrumentRepository */
         $instrumentRepository = app(InstrumentRepositoryInterface::class);
         $instruments = $instrumentRepository
-            ->builder()
-            ->with([
-                'organisation',
-                'implementation',
-                'groupForms',
-                'locations',
-                'registrationCodes',
-                'ratings',
-                'tiles',
-                'targetGroups',
-                'clientCharacteristics',
-                'links',
-                'videos',
-                'downloads',
-                'provider',
-                'contacts',
-                'availableRegions',
-                'availableTownships',
-                'availableNeighbourhoods',
-                'parentInstrument'
-            ])
+            ->getElasticResourceBuilder()
             ->get();
+
+        $index = 'instruments';
+        $delay = 0;
+        $instruments->chunk(SyncBulkResourcesToPublicElasticJob::BATCH_SIZE)->each(function ($instrumentsBatch) use ($index, &$delay) {
+            $syncAttempt = SyncAttemptFactory::makeSyncAttempt(SyncAttempt::ACTION_INDEX)
+                ->setNote('public instruments');
+            $syncAttempt->save();
+
+            dispatch(new SyncBulkResourcesToPublicElasticJob(
+                $instrumentsBatch,
+                $index,
+                InstrumentResource::class,
+                $syncAttempt
+            ))->delay(now()->addSeconds($delay));
+
+            // Verhoog de vertraging met 5 seconden voor de volgende iteratie, maar nooit meer dan 900
+            $delay = min($delay + 5, 900);
+        });
 
         foreach ($instruments as $instrument) {
             $this->getOutput()->write('.');
