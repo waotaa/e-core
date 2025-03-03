06fec1a77160d0c06741655d44d3892662725b79
Wouter Steggerda <woutersteggerda@gmail.com>
2025-03-03 09:38:21 +0100

    also add single implementation option to the original InstrumentResource

--- src/ElasticResources/Original/InstrumentResource.php
+++ src/ElasticResources/Original/InstrumentResource.php
@@ -65,6 +65,7 @@ class InstrumentResource extends ElasticResource
 
             // relations
             'organisation' => OrganisationResource::one($this->organisation),
+            'implementation' => ImplementationResource::one($this->first_implementation), // Temporary to allow api users some time to adjust
             'implementations' => ImplementationResource::many($this->implementations),
             'group_forms' => GroupFormResource::many($this->groupForms),
             'locations' => LocationResource::many($this->locations),
