b596cb3bc6cd9125ba033b2c27d912fc1b075969
Wouter Steggerda <woutersteggerda@gmail.com>
2025-03-12 16:38:24 +0100

    adds efficient query to get InstrumentOwnedEntities by user

--- src/Repositories/Eloquent/InstrumentOwnedEntityRepository.php
+++ src/Repositories/Eloquent/InstrumentOwnedEntityRepository.php
@@ -2,18 +2,43 @@
 
 namespace Vng\EvaCore\Repositories\Eloquent;
 
+use Illuminate\Contracts\Auth\Access\Authorizable;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
+use Vng\EvaCore\Interfaces\IsManagerInterface;
 
 trait InstrumentOwnedEntityRepository
 {
+    public function getQueryOwnedByInstruments(Collection $instruments): Builder
+    {
+        return $this->addRelatedInstrumentsConditions($this->builder(), $instruments);
+    }
+
     public function addRelatedInstrumentsConditions(Builder $query, Collection $instruments)
     {
         return $query->whereIn('instrument_id', $instruments->pluck('id'));
     }
 
-    public function getQueryOwnedByInstruments(Collection $instruments): Builder
+    public function getQueryOwnedByUser(IsManagerInterface $user): Builder
     {
-        return $this->addRelatedInstrumentsConditions($this->builder(), $instruments);
+        return $this->addForUserConditions($this->builder(), $user);
+    }
+
+    /**
+     * @param Builder $query
+     * @param \Vng\EvaCore\Interfaces\IsManagerInterface&Authorizable $user
+     * @return Builder
+     */
+    public function addForUserConditions(Builder $query, IsManagerInterface $user): Builder
+    {
+        if (!$user->can('viewAll', $this->model)) {
+            $organisationIds = $user->getManager()->organisations->pluck('id')->toArray();
+
+            $query->whereHas('instrument', function (Builder $query) use ($organisationIds) {
+                $query->whereIn('organisation_id', $organisationIds);
+            });
+        }
+
+        return $query;
     }
 }
--- src/Repositories/InstrumentOwnedEntityRepositoryInterface.php
+++ src/Repositories/InstrumentOwnedEntityRepositoryInterface.php
@@ -4,9 +4,12 @@ namespace Vng\EvaCore\Repositories;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
+use Vng\EvaCore\Interfaces\IsManagerInterface;
 
 interface InstrumentOwnedEntityRepositoryInterface extends BaseRepositoryInterface
 {
     public function getQueryOwnedByInstruments(Collection $instruments): Builder;
     public function addRelatedInstrumentsConditions(Builder $query, Collection $instruments);
+    public function getQueryOwnedByUser(IsManagerInterface $user): Builder;
+    public function addForUserConditions(Builder $query, IsManagerInterface $user): Builder;
 }
